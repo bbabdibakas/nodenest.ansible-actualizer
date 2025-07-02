178556c077a0f209592bfa3088b96357e7aa8733
Bekzat Abdibakas <babdibakas@yandex.kz>
2025-07-02 13:37:55 +0500

    feat: installed dotenv, added service runAnsiblePlaybook

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello, World!')
+import path from "path";
+import {runAnsiblePlaybook} from "./services/runAnsiblePlaybook";
+
+const start = async () => {
+    const playbookPath = path.join(__dirname, "ansible", "playbooks", "actualizeABS.yml");
+    const ansibleConfig = path.join(__dirname, "ansible", "ansible.cfg");
+
+    try {
+        const data = await runAnsiblePlaybook(playbookPath, ansibleConfig);
+        console.log(data)
+    } catch (error) {
+        console.error('Ошибка при запуске ansible:', error);
+    }
+}
+
+void start()
